02b1ef737c516eb4d6ffc1b206cbb3ce60019f5e
aprag <78740942+aprag@users.noreply.github.com>
2022-03-06 16:25:46 +0100

    Add files via upload

--- tests/end2end_test.py
+++ tests/end2end_test.py
@@ -14,37 +14,31 @@ def test_index(client):
     var = client.get('/')
     assert var.status_code == 200
 
-
-# we want to test the application end to end
-# so firstly we will verify that when we go on the main page, we obtain the main page
-# after that, we will run the application with a sentence with a known result and verify that the entire page is what we want to obtain
-# Also, during the last verification the results of toxicity percentage will be verify as well
-def test_appCall(client):
-    resultfile = open('./homepageresult.txt').read() #this is the file containing the response we are waiting for
-    
-    response = client.get('/')
+def test_home(client):
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.data.decode() == resultfile
+    assert b'<h1>Toxicity</h1>' in response.data
+    assert b'<title>Data Engineering II</title>' in response.data
+    assert b'<h1>Toxicity</h1>' in response.data
+    assert b'The application is a toxicity monitor, where the user inputs a piece of text' in response.data
 
-def test_run(client):
-    resultfile = open('./runpageresult.txt').read()
-    
-    p = {"inputedText":"Hello, my name is Goldy and i want to say to you that i appreciate the fact you are an asshole."}
-    response = client.post("/", data=p)
+def test_resultpage(client):
+    p = {"inputedText":""} # testing function with post, and watch that we are calling the results page
+    response = client.post("/",data=p)
     assert response.status_code == 200
-    assert response.data.decode() == resultfile
+    assert b'<h1>This is the results page.</h1>' in response.data
     
 def test_result(client):
     #the utility of this function is just to detail that we obtain the result we are waiting for
-    p = {"inputedText":"Hello, my name is Goldy and i want to say to you that i appreciate the fact you are an asshole."}
+    p = {"inputedText":"dumb"}
     response = client.post("/", data=p)
     
-    tox = b'Toxicity : 0.93'
-    sevtox = b'Severe Toxicity : 0.03'
-    obs =b'Obscene : 0.84'
+    tox = b'Toxicity : 0.96'
+    sevtox = b'Severe Toxicity : 0.02'
+    obs =b'Obscene : 0.55'
     threat = b'Threat : 0.0'
-    insult = b'Insult : 0.82'
-    idAt = b'Identity attack : 0.01'
+    insult = b'Insult : 0.88'
+    idAt = b'Identity attack : 0.02'
     
     assert tox in response.data
     assert sevtox in response.data
